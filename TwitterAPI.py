fcabb3abb435184a3ed51959e303745160f6e4b2
j0h4x0r <troyal.lee@gmail.com>
2015-05-06 18:53:50 -0400

    Finish twitter API

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -14,6 +14,7 @@ class TwitterAPI(object):
 
 		# settings
 		self.friends_limit = 1000
+		self.tweets_limit = 3200
 
 	def extract_user_info(self, user):
 		res = {}
@@ -25,10 +26,26 @@ class TwitterAPI(object):
 		res['statuses_count'] = user.statuses_count
 		res['description'] = user.description
 		res['profile_image_url'] = user.profile_image_url
-		try:
-			res['status'] = user.status.text.encode(encoding = 'utf-8')
-		except:
-			res['status'] = None
+		return res
+
+	def extract_tweet_info(self, tweet):
+		res = {}
+		res['id'] = tweet.id
+		res['text'] = tweet.text.encode(encoding = 'utf-8')
+		res['created_at'] = tweet.created_at
+		res['user'] = self.extract_user_info(tweet.user)
+		res['geo'] = True if tweet.coordinates else False
+		res['in_reply'] = True if tweet.in_reply_to_status_id else False
+		res['retweet'] = True if hasattr(tweet.retweeted_status) else False
+		res['retweet_count'] = tweet.retweet_count
+		res['favourite_count'] = tweet.favourite_count
+		# entities statistics
+		res['entities'] = {
+			'media': len(tweet.entities.get('media', [])),
+			'urls': len(tweet.entities.get('urls', [])),
+			'user_mentions': [user['id'] for user in tweet.entities.get('user_mentions', [])],
+			'hashtags': [hashtag['text'] for hashtag in tweet.entities.get('hashtags', [])],
+		}
 		return res
 
 	def get_user(self, user_id = None, screen_name = ""):
@@ -50,8 +67,13 @@ class TwitterAPI(object):
 			res.append(self.extract_user_info(friend))
 		return res
 
-	def get_comments(self):
-		### TO DO
-
-	def get_statuses(self):
-		### TO DO
+	def get_statuses(self, user_id = None, screen_name = ""):
+		uid = user_id if user_id else screen_name
+		try:
+			tweets = tweepy.Cursor(self.api.user_timeline, id = uid, count = 200).items(self.tweets_limit)
+		except tweepy.TweepError as e:
+			return []
+		res = []
+		for tweet in tweets:
+			res.append(self.extract_tweet_info(tweet))
+		return res
